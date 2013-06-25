9aee295cf3636a626360f82a859287fccebbad65
unknown <Michael@.(none)>
2013-06-25 00:33:58 -0400

    removed override that put cart before description

---
+++ app/overrides/spree/products/variants/change_variant_button_styles.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path  => "spree/product/_cart_form",
+                     :name => "change_variant_buttons",
+                     :replace => "div#product-variants",
+                     :partial => "spree/products/variants",
+                     :original => '3e847740dc3e7dfgfhh456468fgdj734hskk')
